458831ff1c8d292621282f0c9848679ea8fed514
Timofey <yakubovich96@mail.ru>
2023-06-14 14:26:07 +0300

    sequelize

--- db.js
+++ db.js
@@ -2,15 +2,20 @@
 // импортируем класс Sequelize из модуля sequelize
 const {Sequelize} = require('sequelize')
 
-module.exports = new Sequelize( // экспортируем объект созданый из класса Sequelize
-// все настройки подключения к бд в переменных окружения
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
-    }
+// module.exports = new Sequelize( // экспортируем объект созданый из класса Sequelize
+// // все настройки подключения к бд в переменных окружения
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         dialect: 'postgres',
+//         host: process.env.DB_HOST,
+//         port: process.env.DB_PORT
+//     }
     
-)
+// )
+
+// psql         "postgres://default:kExl0ZB2yUPQ@ep-raspy-tooth-964057.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+// POSTGRES_URL="postgres://default:kExl0ZB2yUPQ@ep-raspy-tooth-964057-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+
+module.exports = new Sequelize('postgres://default:kExl0ZB2yUPQ@ep-raspy-tooth-964057.us-east-1.postgres.vercel-storage.com:5432/verceldb')
